595a3ffc43888531cab8e21cf83cfde12627749d
Vladislav Shkutovich <vladislav.shkutovich@gmail.com>
2022-09-14 18:36:16 +0300

    feat: redux-persist is added

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { App } from '@/components/App'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import { ThemeProvider } from 'styled-components'
 import theme from '@/themes'
-import { store } from '@/store/store'
+import { store, persistor } from '@/store/store'
 
 ReactDOM.render(
 	<Provider store={store}>
 		<ThemeProvider theme={theme}>
-			<App />
+			<PersistGate loading={null} persistor={persistor}>
+				<App />
+			</PersistGate>
 		</ThemeProvider>
 	</Provider>,
 	document.getElementById('root'),
--- src/store/store.js
+++ src/store/store.js
@@ -9,13 +9,26 @@ import locationSliceReducer from '@/store/slices/locationSlice'
 import createSagaMiddleware from 'redux-saga'
 import rootSaga from './sagas'
 
-const sagaMiddleware = createSagaMiddleware()
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 const rootReducer = combineReducers({
 	weatherState: weatherSliceReducer,
 	serviceState: serviceSliceReducer,
 	locationState: locationSliceReducer,
 })
+
+const persistConfig = {
+	key: 'root',
+	storage,
+}
+
+const persistedReducer = persistReducer(
+	persistConfig,
+	rootReducer,
+)
+
+const sagaMiddleware = createSagaMiddleware()
 
 const middleware = [
 	...getDefaultMiddleware({
@@ -26,8 +39,10 @@ const middleware = [
 ]
 
 export const store = configureStore({
-	reducer: rootReducer,
+	reducer: persistedReducer,
 	middleware: middleware,
 })
+
+export const persistor = persistStore(store)
 
 sagaMiddleware.run(rootSaga)
